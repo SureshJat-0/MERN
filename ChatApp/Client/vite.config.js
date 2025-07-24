48828ee43c6f5da610d72acc9e18d9d9a207e11d
Suresh Jat <sureshjatcode@gmail.com>
2025-07-24 09:25:21 +0530

    Fixed

--- ChatApp/Client/vite.config.js
+++ ChatApp/Client/vite.config.js
@@ -6,8 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    proxy: {
-      '/api': 'http://suresh-chat-backend.onrender.com/',
-    }
+    https: true,
   }
 })
